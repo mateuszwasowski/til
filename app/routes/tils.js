694adcfb58df30357eb48aff8203f8c3ab8ba41c
Mateusz Wąsowski <mateusz.wasowski@netguru.pl>
2016-11-28 20:42:57 +0100

    add button

--- app/routes/tils.js
+++ app/routes/tils.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-  return this.store.findAll('til');
+    console.log("hello");
+    return this.store.findAll('til');
+  },
+
+  actions: {
+    createTil(til, desc) {
+      var author = this.store.peekRecord('author', 4);
+      return this.store.createRecord('til', { description: desc, author: author});
+      //const newTil = this.store.createRecord('til', { description: desc, author: author});
+      //newTil.save()
+    }
   }
 });

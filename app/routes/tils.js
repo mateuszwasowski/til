071687d33b59babd0208e7bee04881a44a8653d1
Mateusz Wąsowski <mateusz.wasowski@netguru.pl>
2017-03-27 22:06:22 +0200

    switch to save tils

--- app/routes/tils.js
+++ app/routes/tils.js
@@ -8,9 +8,9 @@ export default Ember.Route.extend({
   actions: {
     createTil(til, desc) {
       var author = this.store.peekRecord('author', 1);
-      return this.store.createRecord('til', { description: desc, author: author})
-    //  const newTil = this.store.createRecord('til', { description: desc, author: author});
-    //  newTil.save()
+    //  return this.store.createRecord('til', { description: desc, author: author})
+      const newTil = this.store.createRecord('til', { description: desc, author: author});
+      newTil.save()
     }
   }
 });

58f178ec7520113738209aacfbd987207e0ad927
Mateusz Wąsowski <mateusz.wasowski@netguru.pl>
2017-03-28 10:33:11 +0200

    another test

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
-import Config from 'til/config/environment';
+import config from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
-  host: "https://api-til.herokuapp.com",
+  host: config.api_host,
 });

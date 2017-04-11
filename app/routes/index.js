6d9934dda26fc71a0e6a61f8894b3f9b3da7770f
Mateusz Wąsowski <mateusz.wasowski@netguru.pl>
2017-04-11 17:02:25 +0200

    add cookies for session

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,10 +10,14 @@ export default Ember.Route.extend({
   actions: {
     createTil(til, desc) {
         let newTil = this.store.createRecord('til', { description: desc });
-        this.store.findRecord('author', this.get("session").currentUser.data.id).then(author => {
+        this.store.findRecord('author', this.get("session").currentUser.id).then(author => {
           newTil.set('author', author);
           newTil.save();
         });
+    },
+    logout(){
+      this.get("session").logout()
+      this.transitionToRoute('/login')
     }
   }
 });
--- app/services/session.js
+++ app/services/session.js
@@ -10,10 +10,26 @@ export default Ember.Service.extend({
   login(){
       this.get('torii').open('google').then(data => {
         this.set('currentUser', data.author);
-        Cookies.set('author', data.author);
+        Cookies.set('authorId', data.author.data.id);
+        this.initializeFromCookie();
       });
   },
   logout(){
-    this.set('currentUser', null);
-  }
+    this.set("currentUser", null);
+    Cookies.remove('authorId');
+  },
+  init(){
+    this._super(...arguments);
+    this.initializeFromCookie();
+  },
+  initializeFromCookie(){
+    var authorId = Cookies.get('authorId');
+    console.log("author id is:");
+    console.log(authorId);
+    if(!!authorId){
+      this.get('store').findRecord('author', authorId).then(author => {
+          this.set('currentUser', author);
+      });
+    }
+  },
 });

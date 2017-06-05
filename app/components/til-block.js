a2359fef6ae627821671444697982b123e678c58
Mateusz Wąsowski <mateusz.wasowski@netguru.pl>
2017-06-05 15:31:40 +0200

    switch gets

--- app/components/til-block.js
+++ app/components/til-block.js
@@ -5,9 +5,9 @@ export default Ember.Component.extend({
   session: Ember.inject.service(),
   actions: {
     vote(til_id){
-      const store = get(this, 'store');
+      const store = this.get("store")
       store.findRecord('til', til_id).then((til) => {
-        store.findRecord('author', get(this, 'session.authorId')).then(author => {
+        store.findRecord('author', this.get("session").authorId).then(author => {
           const newVote = store.createRecord('vote', {
             til: til,
             author: author,

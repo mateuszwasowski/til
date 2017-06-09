5858b22f220bd4656652db97e55c7fda74f86516
Mateusz Wąsowski <mateusz.wasowski@netguru.pl>
2017-06-09 15:29:14 +0200

    check if user hasVote

--- app/components/til-block.js
+++ app/components/til-block.js
@@ -3,12 +3,27 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
   session: Ember.inject.service(),
+  hasVote: Ember.computed('til.votes', function(){
+    const votes_promise = this.get('til.votes');
+    const author_id = this.get("session").authorId;
+    return votes_promise.then(votes_relationship =>  {
+      const votes = votes_relationship.toArray();
+      return votes.some(vote => {
+        return vote.belongsTo('author').id() == author_id;
+      });
+    });
+  }),
   actions: {
-    vote(til){
-      let newVote = this.get('store').createRecord('vote', {til: til});
-      this.get('store').findRecord('author', this.get("session").authorId).then(author => {
-        newVote.set('author', author);
-        newVote.save();
+    vote(til_id){
+      const store = this.get('store');
+      store.findRecord('til', til_id).then(til => {
+        store.findRecord('author', this.get("session").authorId).then(author => {
+          const newVote = store.createRecord('vote', {
+            til: til,
+            author: author,
+          });
+          newVote.save();
+        });
       });
     },
   }
